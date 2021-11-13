acfdf8c8cf51609085b2bb530377a087a1d4ba85
otegal <gyouzakuuka@gmail.com>
2021-11-13 11:58:16 +0900

    resultの簡単なテストを書いた

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use dotenv::dotenv;
-
-fn main() -> Result<(), dotenv::Error> {
-    dotenv().ok();
-    let test_string = read_string_from_dotenv()?;
-    dbg!(test_string);
-    Ok(())
+fn main() {
+    println!("Hello");
 }
 
-fn read_string_from_dotenv() -> Result<String, std::env::VarError> {
-    std::env::var("TEST_STRING")
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_result() {
+        assert_eq!("0".parse::<u8>(), Ok(0));
+        assert_eq!("a".parse::<u8>().unwrap_err().kind(), &std::num::IntErrorKind::InvalidDigit);
+    }
 }
