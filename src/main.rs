e0f7362aedda0836fe4e540956bd1a6b8c7d6d1b
otegal <gyouzakuuka@gmail.com>
2021-11-13 10:54:03 +0900

    複数のエラーを取り扱えなくてコンパイルエラーになるところまで書いた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+fn main() -> Result<(), dotenv::Error> {
+    dotenv().ok();
+    let test_string = read_string_from_dotenv()?;
+    dbg!(test_string);
+    Ok(())
+}
+
+fn read_string_from_dotenv() -> Result<String, std::env::VarError> {
+    std::env::var("TEST_STRING")
 }
